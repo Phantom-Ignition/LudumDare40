d26ffb9c12b1732d338c76e8a5a7eb051e258926
Rafael Almeida <rafaelalmeidatk@gmail.com>
2017-12-02 01:13:31 -0200

    First commit

---
+++ LudumDare40/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LudumDare40
+{
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new GameMain())
+                game.Run();
+        }
+    }
+}
